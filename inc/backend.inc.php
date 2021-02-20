2bc63dbaf5ebd1db2983c5ad9c7f5d40cec14b2b
Tom <32821177+tomtactom@users.noreply.github.com>
2021-02-20 13:54:55 +0100

    Bugfix Test

--- inc/backend.inc.php
+++ inc/backend.inc.php
@@ -162,7 +162,12 @@ if(isset($_POST['sendform'])) {
     } elseif(count(explode('-', $birthdate)) != 3) {
       $msg = "Leider stimmt etwas mit der Formatierung des Geburtsdatum nicht. Schreiben Sie: YYYY-MM-DD";
       $error = true;
-    } elseif(!is_numeric($explode('-', $birthdate)[0]) || !is_numeric($explode('-', $birthdate)[1]) || !is_numeric($explode('-', $birthdate)[2])) {
+    } elseif(
+      !is_numeric(
+        $explode(
+          '-',
+          $birthdate)[0]
+        ) || !is_numeric($explode('-', $birthdate)[1]) || !is_numeric($explode('-', $birthdate)[2])) {
       $msg = "Leider stimmt etwas mit der Formatierung des Geburtsdatum nicht. Schreiben Sie: YYYY-MM-DD";
       $error = true;
     } elseif(!checkdate(int($explode('-', $birthdate)[1]), int($explode('-', $birthdate)[2]), int($explode('-', $birthdate)[0]))) {
