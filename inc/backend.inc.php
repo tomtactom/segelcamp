a220c2b8241ab76c45d12c77d2f136a583645432
Tom <32821177+tomtactom@users.noreply.github.com>
2021-02-20 13:57:06 +0100

    $explode to explode Bugfix

--- inc/backend.inc.php
+++ inc/backend.inc.php
@@ -163,14 +163,10 @@ if(isset($_POST['sendform'])) {
       $msg = "Leider stimmt etwas mit der Formatierung des Geburtsdatum nicht. Schreiben Sie: YYYY-MM-DD";
       $error = true;
     } elseif(
-      !is_numeric(
-        $explode(
-          '-',
-          $birthdate)[0]
-        ) || !is_numeric($explode('-', $birthdate)[1]) || !is_numeric($explode('-', $birthdate)[2])) {
+      !is_numeric(explode('-', $birthdate)[0]) || !is_numeric(explode('-', $birthdate)[1]) || !is_numeric(explode('-', $birthdate)[2])) {
       $msg = "Leider stimmt etwas mit der Formatierung des Geburtsdatum nicht. Schreiben Sie: YYYY-MM-DD";
       $error = true;
-    } elseif(!checkdate(int($explode('-', $birthdate)[1]), int($explode('-', $birthdate)[2]), int($explode('-', $birthdate)[0]))) {
+    } elseif(!checkdate(int(explode('-', $birthdate)[1]), int(explode('-', $birthdate)[2]), int(explode('-', $birthdate)[0]))) {
       $msg = "Leider stimmt etwas mit der Formatierung des Geburtsdatum nicht. Schreiben Sie: YYYY-MM-DD";
       $error = true;
     } elseif(explode('-', $max_birthday)[0] < explode('-', $birthdate)[0]) {
