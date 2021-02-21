78602c38ec5528a04dc3cd8b33da5afdf9832b0e
Tom <32821177+tomtactom@users.noreply.github.com>
2021-02-21 19:29:19 +0100

    Cookie Hinweis

--- index.php
+++ index.php
@@ -286,14 +286,43 @@
 										</article>
 									</div>
 								</section>
-                                <footer>
-                                    <nav>
-                                        <ul>
-                                            <li style="float: left; list-style: none; margin: 20px;"><a href="https://www.lohheider-see.de/impressum" rel="external" target="_blank">Impressum</a></li>
-                                            <li style="float: left; list-style: none; margin: 20px;"><a href="https://www.lohheider-see.de/datenschutzerklaerung" rel="external" target="_blank">Datenschutzerklärung</a></li>
-                                        </ul>
-                                    </nav>
-                                </footer>
+                <footer>
+                    <nav>
+                        <ul>
+                            <li style="float: left; list-style: none; margin: 20px;"><a href="https://www.lohheider-see.de/impressum" rel="external" target="_blank">Impressum</a></li>
+                            <li style="float: left; list-style: none; margin: 20px;"><a href="https://www.lohheider-see.de/datenschutzerklaerung" rel="external" target="_blank">Datenschutzerklärung</a></li>
+                        </ul>
+                    </nav>
+                </footer>
+								<div id="footer-cookie">
+							    <span id="description">
+							      Wir nutzen Cookies, um Ihnen ein besseres Nutzererlebnis zu ermöglichen. Keine Sorge, wir speichern dadurch keine Daten von Ihnen. Mit Nutzung dieser Seite akzeptieren Sie funktionelle Cookies.  <a href="https://www.verbraucherzentrale.de/wissen/digitale-welt/datenschutz/cookies-kontrollieren-und-verwalten-11996#0" target="_blank">Was sind Cookies?</a> - <a href="#" target="_blank">Mehr erfahren</a>
+							    </span>
+							    <span id="accept"><a href="javascript:void(0)" title="Ok, verstanden">Ok, verstanden</a></span>
+							  </div>
+
+							  <script>
+							    /*
+							      www.dakitec.de - Cookie Banner
+							      https://dakitec.de/blog/cookie-banner-selbst-erstellen
+							      Dieses Skript zeigt den Banner an, wenn er noch nicht bestätigt wurde
+							    */
+							    var footerCookie = document.querySelector("#footer-cookie");
+							    var footerCookieAccept = document.querySelector("#accept");
+
+							    if (document.cookie.indexOf("CookieBanner=") == -1) {
+							      footerCookie.style.display = "block";
+							    }
+
+							    footerCookieAccept.onclick = function(e) {
+							      var cookieDate = new Date();
+							      cookieDate.setTime(new Date().getTime() + 31104000000);
+
+							      document.cookie = "CookieBanner = 1; path=/; secure; expires=" + cookieDate.toUTCString();
+
+							      footerCookie.style.display = "none";
+							    };
+							  </script>
 						</div>
 					</div>
 			</div>
