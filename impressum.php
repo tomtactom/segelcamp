dfca665983528afea9e46fdce81c2e665019b23a
Tom <32821177+tomtactom@users.noreply.github.com>
2021-02-24 21:36:28 +0100

    Update impressum.php

---
+++ impressum.php
@@ -0,0 +1,37 @@
+<?php require('./inc/backend.inc.php'); ?>
+<?php $page_title = 'Segelcamp 2021'; ?>
+<?php require('./inc/header.inc.php'); ?>
+<section>
+	<header class="major" id="anmelden">
+		<h2>Impressum</h2>
+	</header>
+  <div class="content" style="max-width: 900px; margin: auto;">
+    <p>
+      <strong>Angaben gemäß § 5 TMG: </strong><br>
+      Seglergemeinschaft Lohheider See e. V.
+      <a href="https://goo.gl/maps/YAVCXPgEQ9k" target="_blank">Orsoyer Allee 121<br>
+      47199 Duisburg-Baerl<br></a><br><br>
+      <strong>Kontakt: </strong>
+      Telefon: <a title="jetzt anrufen" href="tel:0049284180496">+49 2841 80496</a><br>
+      Die Seglergemeinschaft Lohheider See besitzt kein Faxgerät.<br>
+      E-Mail-Adresse: <a href="mailto:info@lohheider-see.de">info@lohheider-see.de</a><br>
+      E-Mail-Adresse: <a href="mailto:segelcamp@lohheider-see.de">segelcamp@lohheider-see.de</a><br>
+      <br>
+      Gläubiger-Identifikationsnummer: DE72ZZZ00000129235<br>
+      DSV Nr.: NW 190<br><br>
+      <strong>Copyright:</strong><br>
+      Die verwendeten Grafiken und Bilder, die Beiträge und Seiten sind urheberrechtlich geschützt.<br>
+      © Seglergemeinschaft Lohheider See e. V.<br>
+      Alle Rechte, auch die der fotomechanischen Wiedergabe, der Vervielfältigung und der Verbreitung, auch teilweise, behält sich die SLS vor.<br>
+      Design by HTML5 UP <a href="https://html5up.net/" target="_blank">html5up.net</a> | @ajlkn (<a href="https://html5up.net/license" target="_blank">Lizenz (Attribution 3.0 Unported (CC BY 3.0))</a>)<br>
+      <br>
+      <strong>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: </strong><br>
+      Tom Aschmann<br>
+      <br>
+      Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:<br>
+      <a href="http://ec.europa.eu/consumers/odr" target="_blank">http://ec.europa.eu/consumers/odr</a><br>
+      Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.<br>
+    </p>
+  </div>
+</section>
+<?php require('./inc/footer.inc.php'); ?>
