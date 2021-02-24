ff0b19550d8afc1aab113359a8c0f7b6820a8654
Tom <32821177+tomtactom@users.noreply.github.com>
2021-02-24 21:14:00 +0100

    change header

--- index.php
+++ index.php
@@ -1,53 +1,6 @@
-<!DOCTYPE HTML><?php require('./inc/backend.inc.php'); ?>
-<html lang="de">
-	<head>
-        <meta charset="utf-8" />
-		<title>Segelcamp 2021 | Seglergemeinschaft Lohheider See e.V.</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-        <meta name="language" content="de">
-        <meta name="keywords" content="Segelcamp, Sommercamp, Ferienangebot, Segeln, Lohheider See">
-        <meta name="audience" content="Kinder, Eltern, Familien, Segler">
-        <meta name="page-type" content="Formular">
-        <meta name="page-topic" content="Ferienfreizeit">
-        <meta name="description" content="Melde dein Kind jetzt für das Segelcamp 2021 an und erfahre weitere Informationen über dieses. Wir freuen uns auf Euch.">
-        <meta name="robots" content="index, follow">
-        <meta http-equiv="language" content="german, de">
-        <meta name="author" content="Tom Aschmann">
-        <meta name="copyright" content="Tom Aschmann">
-        <meta name="publisher" content="Seglergemeinschaft Lohheider See e.V.">
-        <meta name="date" content="2021-02-15">
-        <link rel="apple-touch-icon" sizes="57x57" href="./assets/icons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="./assets/icons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="./assets/icons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="./assets/icons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="./assets/icons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="./assets/icons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="./assets/icons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="./assets/icons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="./assets/icons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="./assets/icons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon-16x16.png">
-        <link rel="manifest" href="./assets/icons/manifest.json">
-        <meta name="msapplication-TileColor" content="#f56a6a">
-        <meta name="msapplication-TileImage" content="./assets/icons/ms-icon-144x144.png">
-        <meta name="theme-color" content="#f56a6a">
-		<link rel="stylesheet" href="./assets/css/main.css">
-	</head>
-	<body class="is-preload">
-			<div id="wrapper">
-					<div id="main">
-						<div class="inner">
-							<header id="header">
-								<span class="logo"><strong>Segelcamp</strong> Seglergemeinschaft Lohheider See e.V.</span>
-								<ul class="icons">
-									<li><a href="https://www.facebook.com/segelvereinsls" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-									<li><a href="https://www.instagram.com/sls_jugend" target="_blank" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-                  <li><a href="https://github.com/tomtactom/segelcamp" target="_blank" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-                  <li><a href="mailto:segelcamp@lohheider-see.de" class="icon far fa-envelope"><span class="label">E-Mail</span></a></li>
-								</ul>
-							</header>
+<?php require('./inc/backend.inc.php'); ?>
+<?php $page_title = 'Segelcamp 2021'; ?>
+<?php require('./inc/header.inc.php'); ?>
 								<section id="banner">
 									<div class="content">
 										<header>
