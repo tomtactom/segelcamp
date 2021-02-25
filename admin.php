94a0d7b30ae5bdc9351971f4c023efb7683661b1
Tom <32821177+tomtactom@users.noreply.github.com>
2021-02-25 19:29:11 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -6,6 +6,7 @@
 		<h2>Administrator Bereich</h2>
 	</header>
   <div class="content" style="max-width: 900px; margin: auto;">
+		<?php if(isset($msg_field)) { echo $msg_field; } ?>
     <?php if($_COOKIE['token'] != $admin_cookie_hash) { ?>
 			<div class="row gtr-uniform">
 				<p>Bitte denken Sie unbedingt daran sich Abzumelden, wenn Sie sich auf einem öffentlichen Computer anmelden.</p>
