ebc08e7e3a4c8880bbddeafd08d9fb1fbbcef9d2
Tom <32821177+tomtactom@users.noreply.github.com>
2021-06-13 19:55:24 +0200

    Update vorschau.php

--- vorschau.php
+++ vorschau.php
@@ -34,7 +34,7 @@
 
 					$total_mails = "";
 					?>
-					<a class="not_print" href="javascript:self.print()" style="display:block;text-decoration:none;float:right;"><img src="/assets/icons/printer.png" alt="Seite Drucken" class="not_print"></a>
+					<a class="not_print" href="javascript:self.print()" style="display:block;text-decoration:none;float:right;border:0;"><img src="/assets/icons/printer.png" alt="Seite Drucken" class="not_print"></a>
 					<?php
 
 					while($row = $statement->fetch()) {
