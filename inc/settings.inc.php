6b2945af1fe6f909db0fc62f1870a500b16d5e4b
Tom <tom.aschmann@t-online.de>
2024-05-26 12:33:08 +0200

    Bugfix: Datum anpassen

--- inc/settings.inc.php
+++ inc/settings.inc.php
@@ -1,7 +1,7 @@
 <?php
   $costs = "150 € (bzw. 120 € für Vereinsmitglieder)";
   $datestart = "29.07.2024";
-  $dateend = "08.08.2024";
+  $dateend = "02.08.2024";
   $timestart = "10:00";
   $timeend = "18:00";
   $whatsappgrouplink = "https://chat.whatsapp.com/LfaZpiiaQ7OErtvmubwEyK";
